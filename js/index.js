117c593ab0fd9f03e84fd6fb4a76fd881397941b
FelipeBarros10 <barrosfelipe735@gmail.com>
2025-01-14 01:09:04 -0300

    Finished

--- js/index.js
+++ js/index.js
@@ -1,21 +1,81 @@
-function addToList () {
+function addToList() {
   const inputText = document.getElementById("inputText");
 
   const listArea = document.getElementById("listArea");
 
   let newTask = inputText.value;
 
-  var li = document.createElement('li');
-  
-  var inputCheckbox = document.createElement("input");
+  if (newTask == "") {
+    alert("Insira a tarefa");
+  } else {
+    var li = document.createElement("li");
 
-  inputCheckbox.setAttribute("type", "checkbox")
+    var inputCheckbox = document.createElement("input");
 
-  li.appendChild(inputCheckbox);
+    inputCheckbox.setAttribute("type", "checkbox");
 
-  console.log(li);
+    li.appendChild(inputCheckbox);
 
+    var text = document.createElement("span");
 
-  
-  
+    text.textContent = newTask;
+
+    li.appendChild(text);
+
+    var selectPriority = document.createElement("select");
+
+    var optionsValue = ["Urgente", "Importante", "Rotina"];
+
+    var options = [];
+
+    optionsValue.forEach((item) => {
+      if (item) {
+        options = document.createElement("option");
+        options.textContent = item;
+
+        selectPriority.appendChild(options);
+      }
+    });
+
+    li.appendChild(selectPriority);
+
+    selectPriority.addEventListener("change", (event) => {
+      selectPriority.style.removeProperty("background-color");
+
+      if (event.target.value === "Urgente") {
+        selectPriority.style.setProperty("background-color", "red");
+      } else if (event.target.value === "Importante") {
+        selectPriority.style.setProperty(
+          "background-color",
+          "rgb(163, 124, 51)"
+        );
+      } else if (event.target.value === "Rotina") {
+        selectPriority.style.setProperty("background-color", "rgb(29, 78, 0)");
+      }
+    });
+
+    var trashBtn = document.createElement("button");
+
+    var iconTrashBtn = document.createElement("i");
+
+    iconTrashBtn.className = "bi bi-trash";
+
+    trashBtn.appendChild(iconTrashBtn);
+
+    li.appendChild(trashBtn);
+
+    listArea.appendChild(li);
+
+    inputCheckbox.addEventListener("change", () => {
+      text.classList.toggle("checked", inputCheckbox.checked);
+
+      li.classList.toggle("li-checked", inputCheckbox.checked);
+    });
+
+    trashBtn.addEventListener("click", () => {
+      li.remove();
+    });
+
+    inputText.value = "";
+  }
 }
